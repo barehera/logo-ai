917039d1e78144ef634599968c7e0cd3ce0d50c2
Çağan Büyükavcılar <mircagi@hotmail.com>
2025-04-12 06:28:21 +0300

    nativewind and react-native-reusables setup

---
+++ providers/FontProvider.tsx
@@ -0,0 +1,21 @@
+import { useFonts } from "expo-font";
+import { SplashScreen } from "expo-router";
+import React, { useEffect } from "react";
+
+export const FontProvider = ({ children }: { children: React.ReactNode }) => {
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return <React.Fragment>{children}</React.Fragment>;
+};
---
+++ providers/ThemeProvider.tsx
@@ -0,0 +1,52 @@
+import React, { useLayoutEffect } from "react";
+import {
+  Theme,
+  ThemeProvider as NavigationThemeProvider,
+  DefaultTheme,
+  DarkTheme,
+} from "@react-navigation/native";
+import { NAV_THEME } from "@/lib/constants";
+import { useColorScheme } from "@/lib/useColorScheme";
+import { StatusBar } from "expo-status-bar";
+
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from "expo-router";
+
+const LIGHT_THEME: Theme = {
+  ...DefaultTheme,
+  colors: NAV_THEME.light,
+};
+const DARK_THEME: Theme = {
+  ...DarkTheme,
+  colors: NAV_THEME.dark,
+};
+
+export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
+  const hasMounted = React.useRef(false);
+  const { colorScheme, isDarkColorScheme } = useColorScheme();
+  const [isColorSchemeLoaded, setIsColorSchemeLoaded] = React.useState(false);
+
+  useLayoutEffect(() => {
+    if (hasMounted.current) {
+      return;
+    }
+
+    setIsColorSchemeLoaded(true);
+    hasMounted.current = true;
+  }, []);
+
+  if (!isColorSchemeLoaded) {
+    return null;
+  }
+
+  return (
+    <NavigationThemeProvider
+      value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}
+    >
+      <StatusBar style={isDarkColorScheme ? "light" : "dark"} />
+      {children}
+    </NavigationThemeProvider>
+  );
+};
